d0cbb32b091953fc1a55a793fc6144ca54e28b25
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-04 16:42:02 -0300

    :+1: Add the first entity classes.

--- movies/Program.cs
+++ movies/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace movies
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class MoviesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Provider=MSOLEDBSQL;Server=.;Database=Movies;UID=sa;PWD=YourStrongPassword1234;");
+        }
+        protected MoviesDbContext()
+        {}
+        public DbSet<Movie> Movies { get; set; }
     }
 }
