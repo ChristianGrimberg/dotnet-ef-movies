a34b3555621d089dd568fda33e466c7d2c96dc6d
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-10 16:40:41 -0300

    :+1: Add qty of registries to persist in data base.

--- movies/Program.cs
+++ movies/Program.cs
@@ -10,8 +10,9 @@ namespace movies
         {
            if(args.Length == 0 || args == null)
            {
-               Movie newMovie = new Movie();
-               MoviesDbContext db = new MoviesDbContext();
+               int result;
+               var newMovie = new Movie();
+               var db = new MoviesDbContext();
 
                Console.Write("Enter the name of the movie: ");
                newMovie.Name = Console.ReadLine();
@@ -20,7 +21,16 @@ namespace movies
                newMovie.Year = int.Parse(Console.ReadLine());
 
                db.Add(newMovie);
-               db.SaveChanges();
+               result = db.SaveChanges();
+
+               if(result != 0)
+               {
+                   Console.WriteLine("Se almacenaron {0} registro/s.", result);
+               }
+               else
+               {
+                   Console.WriteLine("Error al almacenar los datos en la base de datos.");
+               }
            }
            else
            {
