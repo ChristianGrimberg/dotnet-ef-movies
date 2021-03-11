a0b77cb40a55a428c51a8789614fe7d87ea9259c
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-11 15:33:33 -0300

    :+1: Add data annotations to class properties.

--- movies/Movie.cs
+++ movies/Movie.cs
@@ -1,13 +1,17 @@
-using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
 
 namespace movies
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1900,2099)]
         public int Year { get; set; }
     }
 }
--- movies/Program.cs
+++ movies/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace movies
 {
