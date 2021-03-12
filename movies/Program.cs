c2b2af07e49cfd4872a0eb121eefde35c8432a20
Christian Grimberg <christian.grimberg@outlook.com>
2021-03-12 17:06:00 -0300

    :+1: Refactor inserting a Movie into a new static function.

--- movies/Program.cs
+++ movies/Program.cs
@@ -6,24 +6,16 @@ namespace movies
     {
         static void Main(string[] args)
         {
+           int records;
+           var db = new MoviesDbContext();
+
            if(args.Length == 0 || args == null)
            {
-               int result;
-               var newMovie = new Movie();
-               var db = new MoviesDbContext();
+               records = InsertNewMovie(db);
 
-               Console.Write("Enter the name of the movie: ");
-               newMovie.Name = Console.ReadLine();
-
-               Console.Write("Enter the year: ");
-               newMovie.Year = int.Parse(Console.ReadLine());
-
-               db.Add(newMovie);
-               result = db.SaveChanges();
-
-               if(result != 0)
+               if(records != 0)
                {
-                   Console.WriteLine("{0} records were stored.", result);
+                   Console.WriteLine("{0} records were stored.", records);
                }
                else
                {
@@ -32,8 +24,25 @@ namespace movies
            }
            else
            {
-               Console.WriteLine("This application does not accept parameters");
+               Console.WriteLine("This application does not accept parameters.");
            }
+        }
+
+        static int InsertNewMovie(MoviesDbContext db)
+        {
+            int result = 0;
+            var newMovie = new Movie();
+
+            Console.Write("Enter the name of the movie: ");
+            newMovie.Name = Console.ReadLine();
+
+            Console.Write("Enter the year: ");
+            newMovie.Year = int.Parse(Console.ReadLine());
+
+            db.Add(newMovie);
+            result = db.SaveChanges();
+
+            return result;
         }
     }
 }
